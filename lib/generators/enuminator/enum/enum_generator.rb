9c9d35e878f1e87153bed3d723f31e5ffbc4a12b
Walmir <owalmirneto@gmail.com>
2023-03-06 11:02:29 -0300

    chore: rename generator of enumerations

--- lib/generators/enuminator/enum/enum_generator.rb
+++ lib/generators/enuminator/enum/enum_generator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Enuminator
-  class CreateGenerator < Rails::Generators::Base
+  class EnumGenerator < Rails::Generators::Base
     source_root File.expand_path('templates', __dir__)
 
     argument :attributes, type: :hash
